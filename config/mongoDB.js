e74c096159dd2f158d07616369b5bf163b8399be
omar zairi <zairiomar044@gmail.com>
2023-11-03 19:16:38 +0100

    connecting to db

---
+++ config/mongoDB.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+const connectDB = async () => {
+  try {
+    const connect = mongoose.connect(
+      "mongodb+srv://zied:xxaRn9WiPneLaCeQ@cluster0.fp7cycf.mongodb.net/?retryWrites=true&w=majority"
+    );
+    console.log("DB Connected Successfully");
+  } catch (error) {
+    console.log(`Error Connecting to DataBase : ${error}`);
+    process.exit(1);
+  }
+};
+export default connectDB;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from "express";
 import cors from "cors";
+import connectDB from "./config/mongoDB.js";
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.static("public"));
 app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("<center><b>Hello 3ASISOS Welcome To FitApp!</b></center>");
