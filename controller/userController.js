8598672832aaf38553f934944e1f43c59ce45f8f
Zied Meddeb <ziedmeddeb123@outlook.com>
2023-11-18 11:18:26 +0100

    fixed update user1

--- controller/userController.js
+++ controller/userController.js
@@ -57,14 +57,14 @@ userController.put(
     try {
       const user = await userService.updateUser(
         req.params.id,
-        req.body.nom,
-        req.body.prenom,
-        req.body.email,
-        req.body.password,
-        req.body.age,
-        req.body.sex,
-        req.body.poids,
-        req.body.taille
+        req.body
+        
+        
+        
+        
+        
+        
+        
       );
       res.status(200).json(user);
     } catch (error) {
