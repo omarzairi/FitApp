a26ff27db439959644cf1bbcc16b4dffd38f591a
Zied Meddeb <ziedmeddeb123@outlook.com>
2023-12-09 23:40:30 +0100

    fixed notif

--- services/notificaitonService.js
+++ services/notificaitonService.js
@@ -1,10 +1,18 @@
 const NotificationModel=require('../models/Notification');
 const NotificationService={
     async createNotification(notificationData){
-        
-        console.log("new user")
-        const newNotification=await NotificationModel.create(notificationData);
-        return await newNotification.save();
+        console.log("Processing user")
+        let notification = await NotificationModel.findOne({ user: notificationData.user });
+    
+        if (notification) {
+            console.log("User exists, updating fcmToken")
+            notification.fcmToken = notificationData.fcmToken;
+        } else {
+            console.log("New user, creating notification")
+            notification = new NotificationModel(notificationData);
+        }
+    
+        return await notification.save();
     },
     async getNotificationByUserId(userId){
         const notification=await NotificationModel.findOne({user:userId});
